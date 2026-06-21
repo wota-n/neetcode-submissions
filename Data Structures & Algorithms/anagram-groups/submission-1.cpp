2a5064bb1d827fce5c75b14671088e3b4753c69c
wota-n <najmeenajib99@gmail.com>
2026-06-21 19:46:43 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> anagramMap;
+        int n = strs.size();
+
+        for(int i = 0; i < n; ++i){
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            anagramMap[key].push_back(strs[i]);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto &res : anagramMap){
+            result.push_back(res.second);
+        }
+
+        return result;
+    }
+};
